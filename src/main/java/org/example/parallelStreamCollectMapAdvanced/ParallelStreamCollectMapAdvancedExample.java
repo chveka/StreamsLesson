2cfcea91cc3cc53818e39040932bf5089ccbcf93
O.Finnikova <o.finnikova@isimplelab.com>
2026-01-22 09:20:39 +0300

    feat Stream API - агрегация и объединение результатов

---
+++ src/main/java/org/example/parallelStreamCollectMapAdvanced/ParallelStreamCollectMapAdvancedExample.java
@@ -0,0 +1,28 @@
+package org.example.parallelStreamCollectMapAdvanced;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.example.StringTools.log;
+
+public class ParallelStreamCollectMapAdvancedExample {
+    public static void main(String[] args) {
+        List<Student> students = Arrays.asList(
+                new Student("Student1", Map.of("Math", 90, "Physics", 85)),
+                new Student("Student2", Map.of("Math", 95, "Physics", 88)),
+                new Student("Student3", Map.of("Math", 88, "Chemistry", 92)),
+                new Student("Student4", Map.of("Physics", 78, "Chemistry", 85))
+        );
+
+        log("1) Создайте коллекцию студентов, где каждый студент содержит информацию о предметах, которые он изучает, и его оценках по этим предметам");
+        students.forEach(System.out::println);
+        log("");
+
+        log("2) Используйте Parallel Stream для обработки данных и создания Map, где ключ - предмет, а значение - средняя оценка по всем студентам");
+        log(students.parallelStream().flatMap(student -> student.getGrades().entrySet().stream())
+                .collect(Collectors.groupingByConcurrent(Map.Entry::getKey, ConcurrentHashMap::new, Collectors.averagingDouble(Map.Entry::getValue))) + "\n");
+    }
+}
